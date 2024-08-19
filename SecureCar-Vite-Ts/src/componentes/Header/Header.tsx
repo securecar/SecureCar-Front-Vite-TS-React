0b42e2b316c10f844b3573f648f81722ecbfa303
cotete <feliperibeirots@gmail.com>
2024-08-19 16:30:52 -0300

    ajustado o header sem css

--- SecureCar-Vite-Ts/src/componentes/Header/Header.tsx
+++ SecureCar-Vite-Ts/src/componentes/Header/Header.tsx
@@ -1,10 +1,15 @@
 import NavBar from "./NavBar/NavBar"
+import {Link} from "react-router-dom"
+import portoSeguroLogo from "../../assets/imgs/porto-seguro.svg";
+import './header.css'
 
 const Header = ()=>{
 
     return(
-        <header>
+        <header className="header">
+            <Link to={"../../pages/PaginaInicial/PaginaInicial"}><img className="img" src={portoSeguroLogo}></img></Link>
             <NavBar/>
+            <Link to="../../pages/PaginaSuaConta/PaginaSuaConta">Pagina Sua Conta</Link>
         </header>
     )
 }
--- SecureCar-Vite-Ts/src/componentes/Header/NavBar/NavBar.tsx
+++ SecureCar-Vite-Ts/src/componentes/Header/NavBar/NavBar.tsx
@@ -3,13 +3,11 @@ import {Link} from "react-router-dom"
 const NavBar = ()=>{
 
     return (
-        <>
-         <Link to="../../pages/PaginaInicial/PaginaInicial">Pagina Inicial</Link>
+        <div className="main-Links">
          <Link to="../../pages/PaginaFAQ/PaginaFAQ">Pagina FAQ</Link>
-         <Link to="../../pages/PaginaSuaConta/PaginaSuaConta">Pagina Sua Conta</Link>
+         <Link to="../../pages/PaginaChatBot/PaginaChatBot">Pagina ChatBot</Link>
          <Link to="../../pages/PaginaNovidades/PaginaNovidades">Pagina Novidades</Link>
-         <Link to="../../pages/PaginaChatBot/PaginaChatBot">Pagina Chat-Bot</Link>
-        </>
+         </div>
     )
 }
 
