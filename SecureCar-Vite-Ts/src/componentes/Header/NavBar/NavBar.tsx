f55c8df9f03c70dc1d165f76f9dd88c91468b6b1
gustavodscruz <gustavodiasdsc@gmail.com>
2024-08-22 10:35:10 -0300

    tailwind configurado e funcionando, com teste na navbar

--- SecureCar-Vite-Ts/src/componentes/Header/NavBar/NavBar.tsx
+++ SecureCar-Vite-Ts/src/componentes/Header/NavBar/NavBar.tsx
@@ -4,9 +4,9 @@ const NavBar = ()=>{
 
     return (
         <div className="main-Links">
-         <Link to="../../pages/PaginaFAQ/PaginaFAQ">Pagina FAQ</Link>
-         <Link to="../../pages/PaginaChatBot/PaginaChatBot">Pagina ChatBot</Link>
-         <Link to="../../pages/PaginaNovidades/PaginaNovidades">Pagina Novidades</Link>
+         <Link className="text-gray-500 hover:text-purple-600 mx-3" to="../../pages/PaginaFAQ/PaginaFAQ">Pagina FAQ</Link>
+         <Link className="text-gray-500 hover:text-purple-600 mx-3" to="../../pages/PaginaChatBot/PaginaChatBot">Pagina ChatBot</Link>
+         <Link className="text-gray-500 hover:text-purple-600 mx-3" to="../../pages/PaginaNovidades/PaginaNovidades">Pagina Novidades</Link>
          </div>
     )
 }
