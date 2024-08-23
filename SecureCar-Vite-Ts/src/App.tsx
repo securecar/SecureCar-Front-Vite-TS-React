e4df7c39dccb30c6406d7a93c63e741ce34b1ce5
cotete <feliperibeirots@gmail.com>
2024-08-22 21:53:27 -0300

    Ajustado os links de ambas as paginas

--- SecureCar-Vite-Ts/src/App.tsx
+++ SecureCar-Vite-Ts/src/App.tsx
@@ -1,26 +1,14 @@
 import './App.css'
 import Header from './componentes/Header/Header'
-import {Routes, Route} from "react-router-dom";
-import PaginaNovidades from './pages/PaginaNovidades/PaginaNovidades';
-import PaginaChatBot from './pages/PaginaChatBot/PaginaChatBot';
-import PaginaSuaConta from './pages/PaginaSuaConta/PaginaSuaConta';
-import PaginaRegistroLogin from './pages/PaginaRegistroLogin/PaginaRegistroLogin';
-import PaginaInicial from './pages/PaginaInicial/PaginaInicial';
-import PaginaFAQ from './pages/PaginaFAQ/PaginaFAQ';
+import {Outlet} from "react-router-dom";
+
 
 const App = ()=> {
 
   return (
     <>
     <Header/>
-      <Routes>
-        <Route path="/" Component={PaginaRegistroLogin}></Route>
-        <Route path="/pages/PaginaInicial/PaginaInicial" Component={PaginaInicial}></Route>
-        <Route path="/pages/PaginaFAQ/PaginaFAQ" Component={PaginaFAQ}></Route>
-        <Route path="/pages/PaginaNovidades/PaginaNovidades" Component={PaginaNovidades}></Route>
-        <Route path="/pages/PaginaChatBot/PaginaChatBot" Component={PaginaChatBot}></Route>
-        <Route path="/pages/PaginaSuaConta/PaginaSuaConta" Component={PaginaSuaConta}></Route>
-      </Routes>
+    <Outlet/>
     </>
   )
 }
--- SecureCar-Vite-Ts/src/main.tsx
+++ SecureCar-Vite-Ts/src/main.tsx
@@ -1,12 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import PaginaErro from './pages/PaginaErro/PaginaErro.tsx';
+import PaginaRegistroLogin from './pages/PaginaRegistroLogin/PaginaRegistroLogin.tsx';
+import PaginaInicial from './pages/PaginaInicial/PaginaInicial.tsx';
+import PaginaFAQ from './pages/PaginaFAQ/PaginaFAQ.tsx';
+import PaginaNovidades from './pages/PaginaNovidades/PaginaNovidades.tsx';
+import PaginaChatBot from './pages/PaginaChatBot/PaginaChatBot.tsx';
+import PaginaSuaConta from './pages/PaginaSuaConta/PaginaSuaConta.tsx';
+
+
+
+
+const routes = createBrowserRouter([
+  {
+    path:'/',element:<App/>, errorElement:<PaginaErro/>,children:[
+      {path:'/',element:<PaginaRegistroLogin/>},
+      {path:'/pages/PaginaInicial/PaginaInicial',element:<PaginaInicial/>},
+      {path:'/pages/PaginaFAQ/PaginaFAQ',element:<PaginaFAQ/>},
+      {path:'/pages/PaginaNovidades/PaginaNovidades',element:<PaginaNovidades/>},
+      {path:'/pages/PaginaChatBot/PaginaChatBot',element:<PaginaChatBot/>},
+      {path:'/pages/PaginaSuaConta/PaginaSuaConta',element:<PaginaSuaConta/>}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={routes}/>
   </StrictMode>,
 )
