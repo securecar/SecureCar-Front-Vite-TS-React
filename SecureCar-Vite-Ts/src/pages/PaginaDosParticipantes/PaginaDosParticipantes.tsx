b355ebc60c88954f4df551fa25001fa0d89486e3
enricodelguerra <ricodelguerra@gmail.com>
2024-09-04 14:38:12 -0300

    Imagem adicionada ao array integrantes

--- SecureCar-Vite-Ts/src/pages/PaginaDosParticipantes/PaginaDosParticipantes.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaDosParticipantes/PaginaDosParticipantes.tsx
@@ -3,9 +3,34 @@ type Integrantes = {
     rm:number;
     turma:string;
     github:string;
+    src:string;
 }
 
 const PaginaDosParticipantes = ()=>{
+
+const integrantes:Integrantes[] = [
+    {
+        nome: "Felipe Ribeiro Tardochi da Silva",
+        rm: 555100,
+        turma:"1TDSPH",
+        github:"https://github.com/cotete",
+        src:"src"
+    },
+    {
+        nome: "Gustavo Dias da Silva Cruz",
+        rm: 556448,
+        turma:"1TDSPH",
+        github:"github.com/gustavodscruz",
+        src:"src"
+    },
+    {
+        nome: "Enrico Figueiredo Del Guerra",
+        rm: 558604,
+        turma:"1TDSPH",
+        github:"https://github.com/enricodelguerra",
+        src:"src"
+    }
+]
 
     return(
         <>
