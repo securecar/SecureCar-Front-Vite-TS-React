6c93ebccd73bdaf6cf21e6e638e505d9bf30f7a3
cotete <feliperibeirots@gmail.com>
2024-09-03 23:32:57 -0300

    feita a logica de clicar no card de help e abrir ele

--- SecureCar-Vite-Ts/src/pages/PaginaFAQ/componentes/HelpCard/HelpCard.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaFAQ/componentes/HelpCard/HelpCard.tsx
@@ -1,22 +1,34 @@
 import seta from "@/assets/icons/down-arrow-5-svgrepo-com.svg"
+import { useState } from "react";
 
 
 
-type HelpCardProps={
+export type HelpCardProps={
     header : string;
     conteudo : string;
-    mostrar:boolean;
+
 }
 
-const HelpCard = ({header,conteudo,mostrar} : HelpCardProps)=>{
+const HelpCard = ({header,conteudo} : HelpCardProps)=>{
+
+    const[mostrar,setMostrar] = useState<boolean>()
+
+    function clica(){
+        if(mostrar){
+            setMostrar(false)
+            return;
+        }
+        setMostrar(true);
+    }
+
 
     return(
-        <div className={`flex flex-col w-4/5 shadow-2xl ${mostrar? "rounded-xl":""}`}>
-            <div className={`cursor-pointer w-full h-16 flex flex-row justify-between items-center ${mostrar? "rounded-t-xl":"rounded-xl"} bg-primary p-3`}>
-                <h1 className="font-bold text-3xl text-white">{header}</h1>
-                <img className="h-16 invert z-0" src={seta} alt="Imagem para clicar e abrir o card de Help"></img>
+        <div onClick={() =>clica()} className={`select-none  flex flex-col w-4/5 shadow-xl ${mostrar? "rounded-xl":""}`}>
+            <div className={`cursor-pointer w-full h-16 flex flex-row justify-between items-center ${mostrar? "rounded-t-xl":"rounded-xl"} rounded-xl p-3 border-2 border-primary shadow-xl`}>
+                <h1 className="font-bold text-2xl text-black">{header}</h1>
+                <img className="h-16  z-0" src={seta} alt="Imagem para clicar e abrir o card de Help"></img>
             </div>
-            <div className="w-full min-h-32 h-auto flex p-3 text-xl text-black">
+            <div className={`w-full min-h-32 h-auto flex p-3 text-xl text-black ${mostrar?"block":"hidden"}`}>
                 {conteudo}
             </div>
         </div>
