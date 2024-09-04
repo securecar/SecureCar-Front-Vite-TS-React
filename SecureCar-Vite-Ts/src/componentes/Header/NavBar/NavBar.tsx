36978602094ce628fd9bd4e7900a65421fdf4352
cotete <feliperibeirots@gmail.com>
2024-09-04 09:43:58 -0300

    mudado os nomes da nav bar, e mudada a cor do texto de quando a pessoa não possui nenhum carro

--- SecureCar-Vite-Ts/src/componentes/Header/NavBar/NavBar.tsx
+++ SecureCar-Vite-Ts/src/componentes/Header/NavBar/NavBar.tsx
@@ -3,9 +3,9 @@ const NavBar = ()=>{
 
     return (
         <div className="main-Links">
-         <Link className="text-xl text-white hover:text-black mx-3 font-medium " to="../../pages/PaginaFAQ/PaginaFAQ">Pagina FAQ</Link>
-         <Link className="text-xl text-white hover:text-black mx-3 font-medium" to="../../pages/PaginaChatBot/PaginaChatBot">Pagina ChatBot</Link>
-         <Link className="text-xl text-white hover:text-black mx-3 font-medium" to="../../pages/PaginaNovidades/PaginaNovidades">Pagina Novidades</Link>
+         <Link className="text-xl text-white hover:text-black mx-3 font-medium " to="../../pages/PaginaFAQ/PaginaFAQ">Perguntas Frequentes</Link>
+         <Link className="text-xl text-white hover:text-black mx-3 font-medium" to="../../pages/PaginaChatBot/PaginaChatBot">ChatBot</Link>
+         <Link className="text-xl text-white hover:text-black mx-3 font-medium" to="../../pages/PaginaNovidades/PaginaNovidades">Novidades</Link>
          </div>
     )
 }
