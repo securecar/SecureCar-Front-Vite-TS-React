1f297459906353b635ef7577ebeeb3649ef64223
cotete <feliperibeirots@gmail.com>
2024-09-07 18:00:39 -0300

    feita logica pra mudar o media query do perfil

--- SecureCar-Vite-Ts/src/pages/PaginaSuaConta/PaginaSuaConta.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaSuaConta/PaginaSuaConta.tsx
@@ -5,6 +5,7 @@ import Acessibilidade from "./componentes/Sessoes/Acessibilidade/Acessibilidade"
 import SuasConfiguracoes from "./componentes/Sessoes/SuasConfiguracoes/SuasConfiguracoes";
 import Endereco from "./componentes/Sessoes/Endereço/Endereco";
 
+
 const PaginaSuaConta = ()=>{
     
     const [conteudo,setConteudo] = useState("Suas Configuracoes")
@@ -40,8 +41,6 @@ const PaginaSuaConta = ()=>{
 
 
     let listaCarro : [] = []
-
-
     function mudaBotao(nome : string){
         setConteudo(nome)
         setSuasConfiguracoes(nome === "Suas Configuracoes")
@@ -50,6 +49,13 @@ const PaginaSuaConta = ()=>{
         setAcessibilidade(nome === "Acessibilidade")
     }
 
+    function Volta() {
+        setAcessibilidade(true)
+        setCarros(true)
+        setEndereco(true)
+        setSuasConfiguracoes(true)
+        setConteudo("")
+    }
 
     let conteudoChanger = () => {
         switch(conteudo){
@@ -66,16 +72,14 @@ const PaginaSuaConta = ()=>{
     }
 
     return(
-        <div className={`container flex min-w-full ${modo ? "escuro":""}`}>
-            <div className='w-1/4 left bg-primary min-h-screen'>
-                <nav >
-                    <BtnSessoesPerfil clicado = {suasConfiguracoes} onClick ={() => mudaBotao("Suas Configuracoes")} name ="Suas Configurações" />
-                    <BtnSessoesPerfil clicado = {acessibilidade} name="Acessibilidade" onClick={() => mudaBotao("Acessibilidade")} />
-                    <BtnSessoesPerfil clicado = {endereco} name="Endereço" onClick={() => mudaBotao("Endereco")} />
-                    <BtnSessoesPerfil clicado = {carros} name="Carros" onClick={() => mudaBotao("Carros")} />
-                </nav>
+        <div className={`container tablet:flex-col flex min-w-full ${modo ? "escuro":""}`}>
+            <div className='w-1/4 tablet:w-full tablet:flex-row left border-t-4 border-primary-dark  bg-primary tela:min-h-screen tablet:min-h-auto'>
+                <BtnSessoesPerfil clicaImagem={Volta} clicado = {suasConfiguracoes} onClick ={() => mudaBotao("Suas Configuracoes")} name ="Suas Configurações" />
+                <BtnSessoesPerfil clicaImagem={Volta} clicado = {acessibilidade} name="Acessibilidade" onClick={() => mudaBotao("Acessibilidade")} />
+                <BtnSessoesPerfil clicaImagem={Volta} clicado = {endereco} name="Endereço" onClick={() => mudaBotao("Endereco")} />
+                <BtnSessoesPerfil clicaImagem={Volta} clicado = {carros} name="Carros" onClick={() => mudaBotao("Carros")} />
             </div>
-            <div className='w-3/4 flex justify-center right px-10 py-6 h-4/5'>
+            <div className={`tela:w-3/4 ${suasConfiguracoes ||conteudo === "Acessibilidade" || conteudo === "Endereco" || conteudo === "Carros" ? "tablet:flex flex":"tablet:hidden"} tablet:w-full  justify-center right px-10 py-6 h-4/5`}>
                 {conteudoChanger()}
             </div>
 
