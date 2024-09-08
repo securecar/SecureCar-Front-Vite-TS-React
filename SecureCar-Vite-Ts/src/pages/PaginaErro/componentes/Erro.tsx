c37c2cae0977570e0666a12dca03cd2a91e57ac2
enricodelguerra <ricodelguerra@gmail.com>
2024-09-08 11:16:08 -0300

    Estrutura componente erro finalizada

--- SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
@@ -1,5 +1,10 @@
 export default function Erro(){
     return(
-        <></>
+        <div>
+            <h1>OPS! Parece que algo deu errado!</h1>
+            <h1>Você pode tentar resolver isso clicando <br/> em algum dos links acima ou no <br/> botão abaixo!</h1>
+            <button>Inicio</button>
+            <h1>Caso isso não funcione verifique a sua conexão com a iternet!</h1>
+        </div>
     );
 }
