1f65dbc4a8823108d98a95b6f300d14ca16e0950
enricodelguerra <ricodelguerra@gmail.com>
2024-09-08 20:53:06 -0300

    Css Iniciado pagina erro

--- SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function Erro(){
     return(
-        <div>
-            <h1>OPS! Parece que algo deu errado!</h1>
-            <h1>Você pode tentar resolver isso clicando <br/> em algum dos links acima ou no <br/> botão abaixo!</h1>
-            <Link to="/pages/PaginaInicial/PaginaInicial" className="">Inicio</Link>
+        <div className="flex flex-col items-center border  ">
+            <h1 className="text-3xl font-bold">OPS! Parece que algo deu errado!</h1>
+            <h3 className="text-xl font-bold">Você pode tentar resolver isso clicando <br/> em algum dos links acima ou no <br/> botão abaixo!</h3>
+            <Link to="/pages/PaginaInicial/PaginaInicial" className="border rounded-md border-blue-400">Inicio</Link>
             <h1>Caso isso não funcione verifique a sua conexão com a iternet!</h1>
         </div>
     );
