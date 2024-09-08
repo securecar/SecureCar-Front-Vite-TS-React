7b946c53f3c2cedb6d8a7c0149131ca46f6b89d7
enricodelguerra <ricodelguerra@gmail.com>
2024-09-08 20:27:14 -0300

    Botão com link para pagina inicial criado

--- SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Erro(){
     return(
         <div>
             <h1>OPS! Parece que algo deu errado!</h1>
             <h1>Você pode tentar resolver isso clicando <br/> em algum dos links acima ou no <br/> botão abaixo!</h1>
-            <button>Inicio</button>
+            <Link to="/pages/PaginaInicial/PaginaInicial" className="">Inicio</Link>
             <h1>Caso isso não funcione verifique a sua conexão com a iternet!</h1>
         </div>
     );
