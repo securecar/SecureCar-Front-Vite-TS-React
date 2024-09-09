29b88e471843549bcacea076b486714771a6c8ea
enricodelguerra <ricodelguerra@gmail.com>
2024-09-09 09:10:23 -0300

    Página integrantes terminada, botão pagina erro arrumada

--- SecureCar-Vite-Ts/src/pages/PaginaDosParticipantes/componentes/Integrantes/Integrantes.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaDosParticipantes/componentes/Integrantes/Integrantes.tsx
@@ -1,23 +1,25 @@
 type Integrantes = {
-    nome:string;
-    rm:number;
-    turma:string;
-    github:string;
-    src:string;
+    nome: string;
+    rm: number;
+    turma: string;
+    github: string;
+    src: string;
 }
 
 export default function Integrantes({ integrante }: { integrante: Integrantes[] }) {
     return (
-        <div>
+        <div className="">
             <ul>
                 {integrante.map((itg) => (
-                    <li className="grid grid-cols-2" key={itg.rm}>
-                        <img src={itg.src} alt={itg.nome} style={{ width: '200px', height: '200px' }} />
-                        <p>{itg.nome}</p>
-                        <p>RM: {itg.rm}</p>
-                        <p>Turma: {itg.turma}</p>
-                        <p><a href={itg.github} target="_blank" rel="noopener noreferrer">{itg.github}</a></p>
-                    </li>   
+                    <li key={itg.rm} className="flex flex-row items-center space-x-10 pb-4">
+                        <img src={itg.src} alt={itg.nome} style={{ width: '200px', height: '200px' }} className="place-self-start " />
+                        <div>
+                            <p>{itg.nome}</p>
+                            <p >RM: {itg.rm}</p>
+                            <p >Turma: {itg.turma}</p>
+                            <p ><a href={itg.github} target="_blank" rel="noopener noreferrer">{itg.github}</a></p>
+                        </div>
+                    </li>
                 ))}
             </ul>
         </div>
--- SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import Botao from "@/componentes/Botao/Botao";
 
-export default function Erro(){
-    return(
+export default function Erro() {
+    return (
         <div className="flex flex-col items-center border  ">
             <h1 className="text-3xl font-bold">OPS! Parece que algo deu errado!</h1>
-            <h3 className="text-xl font-bold">Você pode tentar resolver isso clicando <br/> em algum dos links acima ou no <br/> botão abaixo!</h3>
-            <Link to="/pages/PaginaInicial/PaginaInicial" className="border rounded-md border-blue-400">Inicio</Link>
+            <h3 className="text-xl font-bold">Você pode tentar resolver isso clicando <br /> em algum dos links acima ou no <br /> botão abaixo!</h3>
+            <Botao />
             <h1>Caso isso não funcione verifique a sua conexão com a iternet!</h1>
         </div>
     );
