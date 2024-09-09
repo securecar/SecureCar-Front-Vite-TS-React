5cf2b4a0509c3dea0ea4466a1f2ba452a0e64cef
enricodelguerra <ricodelguerra@gmail.com>
2024-09-09 15:41:47 -0300

    Texto arrumado

--- SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
@@ -1,14 +1,22 @@
 import Botao from "@/componentes/Botao/Botao";
+import { Link } from "react-router-dom";
 
 export default function Erro() {
     return (
-        <div className="flex flex-col items-center border justify-center space-y-7 h-96  ">
-            <h1 className="text-3xl font-bold">OPS! Parece que algo deu errado!</h1>
-            <h3 className="text-xl font-bold">Você pode tentar resolver isso clicando <br /> em algum dos links acima ou no <br /> botão abaixo!</h3>
-            <div>
+        <div className="flex flex-col items-center justify-center h-96">
+            <div className="flex flex-col items-center justify-center h-80 w-3/4 border border-slate-200 shadow-2xl">
+            <h1 className="text-4xl font-bold pb-10 ">OPS! Parece que algo deu errado!</h1>
+            <p className="text-xl font-bold">Você pode tentar resolver isso clicando</p> 
+            <p className="text-xl font-bold">em algum dos links acima ou no</p> 
+            <p className="text-xl font-bold">botão abaixo!</p>
+            <div className="m-5">
+                <Link to={"/"}>
                 <Botao tipo="button">Inicio</Botao>
+                </Link>
             </div>
-            <h1>Caso isso não funcione verifique a sua conexão com a iternet!</h1>
+            <p className="font-bold text-base">Caso isso não funcione verifique a sua conexão com a iternet!</p>
+        
+        </div>
         </div>
     );
 }
