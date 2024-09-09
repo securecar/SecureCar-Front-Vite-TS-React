4a66280da1d538c7ffaa30148d1c02122c48d5ed
cotete <feliperibeirots@gmail.com>
2024-09-09 11:23:22 -0300

    Feito o registro do usuário

--- SecureCar-Vite-Ts/src/pages/PaginaRegistroLogin/PaginaRegistroLogin.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaRegistroLogin/PaginaRegistroLogin.tsx
@@ -3,29 +3,56 @@ import FormLogin from "./componentes/FormLogin/FormLogin"
 import FormRegistro from "./componentes/FormRegistro/FormRegisto"
 
 
-const PaginaRegistroLogin = ()=>{
+const PaginaRegistroLogin = () => {
 
-    const[conteudo,setConteudo] = useState('Cadastro')
+    const [conteudo, setConteudo] = useState('Cadastro')
 
-    let conteudoChanger = ()=>{
-        switch(conteudo){
+    const lista_user: Usuario[] = []
+
+    type Usuario = {
+        Nome: string;
+        Email: string;
+        Senha: string;
+        CPF: string;
+        Endereco: string;
+        CEP: string;
+        Telefone: string;
+    };
+
+
+    function cadastrar(inputNome: string, inputEmail: string, inputSenha: string, inputSenhaAux: string, inputCPF: string, inputEndereco: string, inputCEP: string, inputTelefone: string):void {
+            const usuario: Usuario = {
+                Nome : inputNome,
+                Email: inputEmail,
+                Senha: inputSenha,
+                CPF: inputCPF,
+                Endereco : inputEndereco,
+                CEP: inputCEP,
+                Telefone: inputTelefone
+            };
+            lista_user.push(usuario)
+            console.log(usuario)
+    }
+
+    let conteudoChanger = () => {
+        switch (conteudo) {
             case 'Cadastro':
-                return <FormRegistro/>
+                return <FormRegistro onSubmit={cadastrar} />
             case 'Login':
-                return <FormLogin/>
+                return <FormLogin />
         }
     }
-    
 
-    return(
-        <div className="h-screen flex flex-col items-center mt-6 w-full pb-20">
-        <div className="border-2 rounded-xl shadow-xl p-4 h-auto w-[30rem]">
-            <div className="flex justify-around">
-            <button className={`text-lg font-semibold w-2/5 rounded-xl p-3 border-2 border-primary shadow-xl hover:bg-primary hover:text-white ${conteudo == "Cadastro" ? "bg-primary text-white":""}`} onClick={()=>setConteudo('Cadastro')}>Cadastrar-se</button>
-            <button className={`text-lg font-semibold w-2/5 rounded-xl p-3 border-2 border-primary shadow-xl hover:bg-primary hover:text-white ${conteudo == "Login" ? "bg-primary text-white":""}`} onClick={()=>setConteudo('Login')}>Acessar Conta</button>
+
+    return (
+        <div className="h-auto flex flex-col items-center mt-6 w-full pb-20">
+            <div className="border-2 rounded-xl shadow-xl p-4 h-auto tela:w-[35rem] tablet:w-[26rem]">
+                <div className="flex justify-around">
+                    <button className={`text-lg font-semibold w-2/5 rounded-xl p-3 border-2 border-primary shadow-xl hover:bg-primary hover:text-white ${conteudo == "Cadastro" ? "bg-primary text-white" : ""}`} onClick={() => setConteudo('Cadastro')}>Cadastrar-se</button>
+                    <button className={`text-lg font-semibold w-2/5 rounded-xl p-3 border-2 border-primary shadow-xl hover:bg-primary hover:text-white ${conteudo == "Login" ? "bg-primary text-white" : ""}`} onClick={() => setConteudo('Login')}>Acessar Conta</button>
+                </div>
+                {conteudoChanger()}
             </div>
-            {conteudoChanger()}
-        </div>
         </div>
     )
 }
--- SecureCar-Vite-Ts/src/pages/PaginaRegistroLogin/componentes/FormRegistro/FormRegisto.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaRegistroLogin/componentes/FormRegistro/FormRegisto.tsx
@@ -1,31 +1,117 @@
-import Botao from "../../../../componentes/Botao/Botao"
-import InputArea from "../../../../componentes/InputArea/InputArea"
+import { useState } from "react";
+import Botao from "../../../../componentes/Botao/Botao";
+import InputArea from "../../../../componentes/InputArea/InputArea";
 
 
+type FormRegistroProps = {
+  onSubmit: (inputNome: string, inputEmail: string, inputSenha: string, inputSenhaAux: string, inputCPF: string, inputEndereco: string, inputCEP: string, inputTelefone: string) => void;
+};
+
+const FormRegistro = ({ onSubmit }: FormRegistroProps) => {
+
+  const [inputNome, setInputNome] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputSenha, setInputSenha] = useState("");
+  const [inputSenhaAux, setInputSenhaAux] = useState("");
+  const [inputCPF, setInputCPF] = useState("");
+  const [inputEndereco, setInputEndereco] = useState("");
+  const [inputCEP, setInputCEP] = useState("");
+  const [inputTelefone, setInputTelefone] = useState("");
 
 
-const FormRegistro = ()=>{
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    return(
-        <>
-        <fieldset>
-            <form className="gap-0.5 flex flex-col">
-                <InputArea placeHolder="Digite seu nome completo" label="Nome Completo" required={true}/>
-                <InputArea placeHolder="Digite seu email" label="Email" required={true}/>
-                <InputArea placeHolder="Digite sua senha" label="Senha" required={true}/>
-                <InputArea placeHolder="Confirme sua senha" label="Confirmar Senha" required={true}/>
-                <InputArea placeHolder="Digite seu CPF" label="Cpf" required={true}/>
-                <InputArea placeHolder="Coloque sua data de Nascimento" label="Data de Nascimento" required={true}/>
-                <InputArea placeHolder="Digite seu Cep" label="CEP" required={true}/>
-                <InputArea placeHolder="Escolha seu Estado" label="Estado" required={true}/>
-                <InputArea placeHolder="Escolha sua Cidade" label="Cidade" required={true}/>
-                <div className="p-3 w-full flex items-center justify-center">
-                    <Botao tipo="submit">Cadastre-se</Botao>
-                </div>
-            </form>
-        </fieldset>
-        </>
-    )
-}
+    
+    function verificaEmail(inputEmail: string): boolean {
+        if(inputEmail.includes("@")){
+            return true
+        }else{
+            return false
+        }
+      }
 
-export default FormRegistro
+    if(inputSenhaAux == inputSenha && verificaEmail(inputEmail) && inputCEP.length==9 && inputCPF.length==11){
+        onSubmit(inputNome, inputEmail, inputSenha, inputSenhaAux, inputCPF, inputEndereco, inputCEP, inputTelefone);
+        setInputNome("");
+        setInputEmail("");
+        setInputSenha("");
+        setInputSenhaAux("");
+        setInputCPF("");
+        setInputEndereco("");
+        setInputCEP("");
+        setInputTelefone("");
+    }else{
+        console.log("Algo de errado aconteceu.")
+    }
+    
+  };
+
+  return (
+    <fieldset>
+      <form className="gap-0.5 flex flex-col" onSubmit={handleSubmit}>
+        <InputArea
+          value={inputNome}
+          required={true}
+          onChange={valor => setInputNome(valor)}
+          label="Nome Completo"
+          placeHolder="Digite seu nome completo"
+        />
+        <InputArea
+          value={inputEmail}
+          required={true}
+          onChange={valor => setInputEmail(valor)}
+          label="Email"
+          placeHolder="Digite seu email"
+        />
+        <InputArea
+          value={inputSenha}
+          required={true}
+          onChange={valor => setInputSenha(valor)}
+          label="Senha"
+          placeHolder="Digite sua senha"
+        />
+        <InputArea
+          value={inputSenhaAux}
+          required={true}
+          onChange={valor => setInputSenhaAux(valor)}
+          label="Confirmar Senha"
+          placeHolder="Confirme sua senha"
+        />
+        <InputArea
+          value={inputCPF}
+          required={true}
+          onChange={valor => setInputCPF(valor)}
+          label="CPF"
+          placeHolder="Digite seu CPF"
+        />
+        <InputArea
+          value={inputEndereco}
+          required={true}
+          onChange={valor => setInputEndereco(valor)}
+          label="Endereço"
+          placeHolder="Digite seu Endereço"
+        />
+        <InputArea
+          value={inputCEP}
+          required={true}
+          onChange={valor => setInputCEP(valor)}
+          label="CEP"
+          placeHolder="Digite seu CEP"
+        />
+        <InputArea
+          value={inputTelefone}
+          required={true}
+          onChange={valor => setInputTelefone(valor)}
+          label="Telefone"
+          placeHolder="Digite seu Telefone"
+        />
+        <div className="p-3 w-full flex items-center justify-center">
+          <Botao tipo="submit">Cadastre-se</Botao>
+        </div>
+      </form>
+    </fieldset>
+  );
+};
+
+export default FormRegistro;
