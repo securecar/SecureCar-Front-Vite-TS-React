311ab59ee4845b0e584d5527a29690a76a0dc040
enricodelguerra <ricodelguerra@gmail.com>
2024-09-09 15:51:49 -0300

    Estrutura de texto da pághina erro finalizada

--- SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 export default function Erro() {
     return (
-        <div className="flex flex-col items-center justify-center h-96">
-            <div className="flex flex-col items-center justify-center h-80 w-3/4 border border-slate-200 shadow-2xl">
-            <h1 className="text-4xl font-bold pb-10 ">OPS! Parece que algo deu errado!</h1>
-            <p className="text-xl font-bold">Você pode tentar resolver isso clicando</p> 
-            <p className="text-xl font-bold">em algum dos links acima ou no</p> 
-            <p className="text-xl font-bold">botão abaixo!</p>
-            <div className="m-5">
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="flex flex-col items-center justify-center h-5/6 w-3/4 border border-slate-200 shadow-2xl">
+            <h1 className="text-5xl font-bold pb-20 ">OPS! Parece que algo deu errado!</h1>
+            <p className="text-2xl font-bold">Você pode tentar resolver isso clicando</p> 
+            <p className="text-2xl font-bold">em algum dos links acima ou no</p> 
+            <p className="text-2xl font-bold">botão abaixo!</p>
+            <div className="m-10">
                 <Link to={"/"}>
                 <Botao tipo="button">Inicio</Botao>
                 </Link>
             </div>
-            <p className="font-bold text-base">Caso isso não funcione verifique a sua conexão com a iternet!</p>
+            <p className="font-bold text-xl">Caso isso não funcione verifique a sua conexão com a iternet!</p>
         
         </div>
         </div>
