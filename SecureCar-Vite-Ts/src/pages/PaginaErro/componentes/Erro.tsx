4885e6a3a43ad32c54dc5f52cd126f17017e54c6
enricodelguerra <ricodelguerra@gmail.com>
2024-09-10 11:39:50 -0300

    Padding h1 arrumado

--- SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
@@ -5,7 +5,7 @@ export default function Erro() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <div className="flex flex-col items-center justify-center h-5/6 w-3/4 border border-slate-200 shadow-2xl">
-            <h1 className="text-5xl font-bold pb-20 ">OPS! Parece que algo deu errado!</h1>
+            <h1 className="text-5xl font-bold pb-16 ">OPS! Parece que algo deu errado!</h1>
             <p className="text-2xl font-bold">Você pode tentar resolver isso clicando</p> 
             <p className="text-2xl font-bold">em algum dos links acima ou no</p> 
             <p className="text-2xl font-bold">botão abaixo!</p>
