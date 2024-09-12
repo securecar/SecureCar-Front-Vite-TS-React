507df4a530d0a233742556ef4eecb9cb0537d8f9
enricodelguerra <ricodelguerra@gmail.com>
2024-09-12 11:55:19 -0300

    Estrutura arrumada e Responsividade tablet iniciada

--- SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaErro/componentes/Erro.tsx
@@ -5,11 +5,8 @@ export default function Erro() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <div className="flex flex-col items-center justify-center w-5/6 border h-5/6 border-slate-200 shadow-2xl">
-                <h1 className="text-4xl font-bold ">OPS! Parece que algo</h1>
-                <h1 className="text-4xl font-bold pb-20">deu errado!</h1>
-                <p className="text-xl font-bold">Você pode tentar resolver isso</p> 
-                <p className="text-xl font-bold">clicando em algum dos links acima </p> 
-                <p className="text-xl font-bold">ou no botão abaixo!</p>
+                <h1 className="text-4xl font-bold text-center m-10 md:text-5xl">OPS! Parece que algo deu errado!</h1>
+                <p className="text-xl font-bold text-center">Você pode tentar resolver isso clicando em algum dos links acima ou no botão abaixo!</p>
                 <iframe className="m-3" src="https://lottie.host/embed/4b5ecd27-58e4-452e-957d-75e9dbf3fba7/nMlkBxD8SR.json"></iframe>
                 <div className="pb-10">
                     <Link to={"/"}>
