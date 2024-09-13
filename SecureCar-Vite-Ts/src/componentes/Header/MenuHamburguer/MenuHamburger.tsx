1aa32c5f895126f7d53f4fe159bf10d0ddb1dba3
cotete <feliperibeirots@gmail.com>
2024-09-13 11:55:11 -0300

    menu hamburgao

---
+++ SecureCar-Vite-Ts/src/componentes/Header/MenuHamburguer/MenuHamburger.tsx
@@ -0,0 +1,36 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NavBar from "../NavBar/NavBar";
+import { Link, useLocation } from "react-router-dom";
+import profile from "@/assets/icons/profile-pic.svg";
+import { useEffect } from "react";
+
+
+
+type MenuProps = {
+    onClick:()=>void;
+    clicado : boolean;
+    setClicado: (value:boolean)=>void
+}
+
+
+const MenuHamburguer = ({onClick,clicado,setClicado} : MenuProps) =>{
+    const login = sessionStorage.getItem('Logado') 
+    const location = useLocation(); 
+
+    useEffect(() => {
+        setClicado(false);
+    }, [location.pathname]);
+
+    return(
+        <div className={clicado?`block w-screen h-screen`:""}>
+            <FontAwesomeIcon onClick={onClick} className="cursor-pointer hover:scale-105 transition-all duration-300 mb-4 p-4" color="#fff" size="2xl" icon={faBars} />
+            <div className="flex w-full flex-col justify-center items-start">
+                <Link className={clicado?`static flex text-xl text-white hover:text-black mx-3 font-medium w-max gap-4 items-center py-4`:"hidden"} to={login ? "/conta" : '/login'}>Seu perfil<img src={profile} className="h-[30px] hover:scale-110 transition"></img></Link>
+                <NavBar isMenuHamburguer={clicado}></NavBar>
+            </div>
+        </div>
+    )
+}
+
+export default MenuHamburguer
--- SecureCar-Vite-Ts/src/componentes/Header/NavBar/NavBar.tsx
+++ SecureCar-Vite-Ts/src/componentes/Header/NavBar/NavBar.tsx
@@ -1,12 +1,16 @@
 import {Link} from "react-router-dom"
-const NavBar = ()=>{
+type NavBarProps = {
+    isMenuHamburguer? : boolean
+}
+
+const NavBar = ({isMenuHamburguer} : NavBarProps)=>{
 
     return (
-        <div className="main-Links">
-         <Link className="text-xl text-white hover:text-black mx-3 font-medium " to="/faq">Perguntas Frequentes</Link>
-         <Link className="text-xl text-white hover:text-black mx-3 font-medium" to="/chatbot">ChatBot</Link>
-         <Link className="text-xl text-white hover:text-black mx-3 font-medium" to="/blog">Novidades</Link>
-         <Link className="text-xl text-white hover:text-black mx-3 font-medium" to={"/participantes"}> EQUIPE </Link>
+        <div className={`${isMenuHamburguer ? ' flex flex-col gap-2 w-full gap-y-5' : 'tablet:hidden'}`}>
+            <Link className="text-xl text-white hover:text-black mx-3 font-medium" to="/chatbot">ChatBot</Link>
+            <Link className="text-xl text-white hover:text-black mx-3 font-medium " to="/faq">Perguntas Frequentes</Link>
+            <Link className="text-xl text-white hover:text-black mx-3 font-medium" to="/blog">Novidades</Link>
+            <Link className="text-xl text-white hover:text-black mx-3 font-medium" to={"/participantes"}> Equipe </Link>
          </div>
     )
 }
