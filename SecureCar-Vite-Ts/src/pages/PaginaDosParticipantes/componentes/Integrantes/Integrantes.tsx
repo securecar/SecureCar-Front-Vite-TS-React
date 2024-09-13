41c814f4d3594f13b99228a00ebfc30edf43b17b
enricodelguerra <ricodelguerra@gmail.com>
2024-09-13 09:38:30 -0300

    Paagina integrantes responsividade tablet terminada

--- SecureCar-Vite-Ts/src/pages/PaginaDosParticipantes/componentes/Integrantes/Integrantes.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaDosParticipantes/componentes/Integrantes/Integrantes.tsx
@@ -11,8 +11,8 @@ export default function Integrantes({ integrante }: { integrante: Integrantes[] 
         <div className="w-4/6">
             <ul>
                 {integrante.map((itg) => (
-                    <li key={itg.rm} className="flex flex-row items-center space-x-10 pb-4 pl-3">
-                        <img src={itg.src} alt={itg.nome} className="w-1/3" />
+                    <li key={itg.rm} className="flex flex-row items-center space-x-10 pb-4 pl-3 pt-5 text-3xl">
+                        <img src={itg.src} alt={itg.nome} className="w-1/3 lg:w-3/5" />
                         <div>
                             <p>{itg.nome}</p>
                             <p >RM: {itg.rm}</p>
