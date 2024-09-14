ebf918229a33b997ff4755aef8b47c28fa22df0b
gustavodscruz <gustavodiasdsc@gmail.com>
2024-09-14 13:08:42 -0300

    alteracao pagina participantes mobile e criacao de novo breakpoint

--- SecureCar-Vite-Ts/src/pages/PaginaDosParticipantes/componentes/Integrantes/Integrantes.tsx
+++ SecureCar-Vite-Ts/src/pages/PaginaDosParticipantes/componentes/Integrantes/Integrantes.tsx
@@ -8,16 +8,16 @@ type Integrantes = {
 
 export default function Integrantes({ integrante }: { integrante: Integrantes[] }) {
     return (
-        <div className="w-4/6">
+        <div className="w-full celular:m-auto">
             <ul>
                 {integrante.map((itg) => (
-                    <li key={itg.rm} className="flex flex-row items-center space-x-10 pb-4 pl-3 pt-5 text-3xl">
-                        <img src={itg.src} alt={itg.nome} className="w-1/3 lg:w-3/5" />
-                        <div>
-                            <p>{itg.nome}</p>
-                            <p >RM: {itg.rm}</p>
-                            <p >Turma: {itg.turma}</p>
-                            <p ><a href={itg.github} target="_blank" rel="noopener noreferrer">{itg.github}</a></p>
+                    <li key={itg.rm} className="flex celular:flex-col flex-row items-center space-x-10 pb-4 celular:pl-0 celular:space-x-0 pl-3 pt-5 text-3xl">
+                        <img src={itg.src} alt={itg.nome} className="w-1/3 lg:w-3/5  celular:rounded-full celular:h-40 celular:w-40 celular:mb-2 " />
+                        <div className="w-full ">
+                            <p className="text-center font-semibold text-wrap max-w-screen-sm text-lg" >{itg.nome}</p>
+                            <p className="text-center text-wrap max-w-screen-sm text-lg" >RM: {itg.rm}</p>
+                            <p className="text-center text-wrap max-w-screen-sm text-lg" >Turma: {itg.turma}</p>
+                            <p className="text-center text-wrap max-w-screen-sm text-lg" ><a href={itg.github} target="_blank" rel="noopener noreferrer">{itg.github}</a></p>
                         </div>
                     </li>
                 ))}
