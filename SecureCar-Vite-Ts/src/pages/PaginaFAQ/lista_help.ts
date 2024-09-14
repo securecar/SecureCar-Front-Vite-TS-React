143dc2a5cdf46f57c8701bdfd76ec4e79a5479f0
gustavodscruz <gustavodiasdsc@gmail.com>
2024-09-14 14:27:59 -0300

    faq com mais pergunta e fix na altura

--- SecureCar-Vite-Ts/src/pages/PaginaFAQ/lista_help.ts
+++ SecureCar-Vite-Ts/src/pages/PaginaFAQ/lista_help.ts
@@ -10,17 +10,17 @@ const lista_help : HelpCardProps[]=[
         header:"Como posso mudar minhas informações pessoas?",
         conteudo:"Para mudar suas informações pessoais, basta entrar no perfil e logo quando abrir você verá uma imagem de um lápis, basta clicar nela que será liberado para que você mude alguma informação. Para alterar o endereço é o mesmo processo só que na aba de endereço."
     },{
-        header:"",
-        conteudo:""
+        header:"Gostei do trabalho de vocês! Como faço para contatar quem fez o site?",
+        conteudo:"Você pode ir na página principal e visitar o Github de cada participante, ou ir na página de participantes do projeto. Esperamos ajudar com o que for preciso!"
     },{
-        header:"",
-        conteudo:""
+        header:"Quero ter mais benefícios da Porto, o que faço?",
+        conteudo:"Você pode acessar o site www.portoseguro.com.br e ter acesso ao benefício do nosso seguro. Você com certeza pode contar conosco, para o que der e vier!"
     },{
-        header:"",
-        conteudo:""
+        header:"Como posso ficar sabendo dos novos recursos do site?",
+        conteudo:"Temos nossa seção \"Novidades\" e lá você ter informações sobre recursos novos, por meio do contato com artigos feitos diretamente pelos nossos criadores. "
     },{
-        header:"",
-        conteudo:""
+        header:"Onde posso ter informações sobre segurança veicular?",
+        conteudo:"Tudo isso e muito mais está na nossa seção \"Novidades \" no cabeçalho do site. Espero ter ajudado :) "
     }
     
 ]
